ada2b1f1d5896a12bf27e20105574283b1181c4a
Joseph Polsky <jhpolsky@gmail.com>
2026-04-16 00:56:24 -0400

    add fake creds to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,3 +2,6 @@ import os
 os.environ['TABLE_NAME'] = 'resume-visitor-count'
 os.environ['ALLOWED_ORIGIN'] = 'https://jhpolsky.com'
 os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
+os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
+os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
+os.environ['AWS_SESSION_TOKEN'] = 'testing'
